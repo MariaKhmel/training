3c9708e41f427f9c49439c88583c90a6ea6cd394
MariaKhmel <mariakhmel8@gmail.com>
2022-04-22 22:21:29 +0300

    training

---
+++ js/training.js
@@ -0,0 +1,15 @@
+// const max = 50;
+// const min = 23;
+
+// let totalSum = 0;
+
+// for (let i = max; i >= min; i-=1) {
+    
+//     console.log(i);
+//     if (i%2 === 0) {
+//         totalSum += i;
+//     }
+// }
+// console.log(totalSum);
+
+
